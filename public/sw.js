dc1a8c925a48d86452f5b8555241afe992149c9d
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-12-12 08:48:05 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -3,19 +3,30 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  // Add more static assets
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
+});
+
+// Push notifications
+self.addEventListener('push', e => {
+  const options = {
+    body: e.data ? e.data.text() : 'PantryPal Alert',
+    icon: '/icon-192.png',
+    badge: '/icon-192.png',
+    vibrate: [100, 50, 100],
+    data: { dateOfArrival: Date.now(), primaryKey: 1 }
+  };
+  e.waitUntil(self.registration.showNotification('PantryPal', options));
 });
